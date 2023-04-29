04e8afdf8208f6b68a3ca51aa9c52c193be21957
dgarls <dgarls.business@gmail.com>
2023-04-29 17:30:42 +0000

    swag

---
+++ mirbetter.py
@@ -0,0 +1,26 @@
+total_input = input()
+total_input = total_input.upper()
+cypher = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+res = []
+word = []
+rep = ''
+w = 0
+for i in total_input:
+    if i.isnumeric():
+        res.append(i)
+    else:
+        word.append(i)
+while w < 2:
+    rep += res.pop(0)
+    w +=1
+del word[0]
+rep = int(rep)
+repeat = 10
+i = 0
+final = ""
+for x in word:
+    for idx, c in enumerate(cypher):
+        if x == c:
+            final += cypher[(idx+2**i) % 26]
+            i +=1
+print(final)
