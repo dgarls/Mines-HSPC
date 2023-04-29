eea8e5c1117ab95fa4a89a288252e1d813cd7dd9
dgarls <dgarls.business@gmail.com>
2023-04-29 11:29:05 -0600

    dada

--- compass.py
+++ compass.py
@@ -1,1 +1,22 @@
 numBearings = int(input())
+directions = {"N": 0, "E": 90, "S" : 180, "W": 270}
+
+for bearing in range(numBearings):
+    bear = [*(input()[::-1])]
+    compass = directions[bear[0]]
+
+    if len(bear) == 1:
+        print(compass)
+        continue
+
+    for direction in bear[1:]:
+        newDirection = directions[direction]
+        if compass >= 180 and direction == "N":
+            newDirection = 360
+        compass = (compass + newDirection) / 2
+        print(f"Direction: {direction}, Compass: {compass}")
+        
+    print(compass)
+
+
+
--- mir.py
+++ mir.py
@@ -3,9 +3,10 @@ import math
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 inpt = input().split()
 code = [*inpt[1]]
+decoded = ''
 
 for idx, char in enumerate(code):
     newIdx = int(alphabet.index(char) + math.pow(2, idx))
-    print(alphabet[newIdx % 26], end='')
+    decoded += alphabet[newIdx % 26]
 
-# Wrong answer
+print(decoded)
